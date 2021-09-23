c5f32ef7729054b84b2d0db137f5826af4308973
relhawary <roeyaelhawary@aucegypt.edu>
2021-09-23 03:13:28 +0200

    working!adds active class when section in viewport

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -19,7 +19,8 @@
  */
 var allSectionElements = document.getElementsByTagName("section");
 var navBarUlElement = document.getElementById("navbar__list");
-
+//var theSectionsArray=[document.getElementById('section1'),document.getElementById('section2'),document.getElementById('section3'),document.getElementById
+//('section4')]
 /**
  * End Global Variables
  * Start Helper Functions
@@ -46,6 +47,7 @@ function buildNavMenu(sections) {
 			menuListItem.appendChild(menuAnchor);
 			navBarUlElement.appendChild(menuListItem);
 			//	console.log("im working bitch");
+			// change this later to use innerHTML with concatination
 		}
 	}
 }
@@ -53,6 +55,38 @@ function buildNavMenu(sections) {
 document.body.onload = buildNavMenu(allSectionElements);
 
 // Add class 'active' to section when near top of viewport
+
+function checkSectionInView(element) {
+	var rectangle = element.getBoundingClientRect();
+	console.log(-rectangle.height);
+	return (
+		// rectangle.top >= 0 &&
+		// rectangle.left >= 0 &&
+		// rectangle.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+		// rectangle.right <= (window.innerWidth || document.documentElement.clientWidth)
+		rectangle.top <= 0 && -rectangle.height <= rectangle.top
+	);
+}
+
+function scrollHandler() {
+	for (let i = 1; i <= allSectionElements.length; i++) {
+		let ID = "section" + i;
+		let idBefore = "section" + (i - 1);
+		let element = document.getElementById(ID);
+		let visible = checkSectionInView(element);
+		if (visible == true) {
+			//document.getElementById(idBefore).classList.remove("acive");
+			element.classList.add("your-active-class");
+		} else {
+			element.classList.remove("your-active-class");
+		}
+		console.log(i, ID, visible, element.getBoundingClientRect());
+	}
+}
+
+document.addEventListener("scroll", function (e) {
+	scrollHandler();
+});
 
 // Scroll to anchor ID using scrollTO event
 
